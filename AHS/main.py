4b78a6052e6841260e445b46098d7c42042598f7
Prajwal Srinivas <60974640+prajwal2308@users.noreply.github.com>
2025-04-11 21:46:11 -0400

    Update main.py

--- AHS/main.py
+++ AHS/main.py
@@ -245,4 +245,6 @@ def upload():
     return render_template('upload.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
